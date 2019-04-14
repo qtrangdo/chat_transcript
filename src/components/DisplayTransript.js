13a22f7abd94acdd760fe64f960838083a62eec9
td94 <quynhtrangdo94@gmail.com>
2019-04-13 20:44:20 -0400

    Add triangle to message bubble

--- src/components/DisplayTransript.js
+++ src/components/DisplayTransript.js
@@ -47,24 +47,30 @@ class DisplayTranscript extends Component {
         {!!conversationDate && <Header chatDate={conversationDate} />}
         <hr/>
         {messages.map((message, i) => {
-          let position ='', focus = '', userColor = '';
+          let position ='', focus = '', userColor = '', triangle = '';
           if (message.username === firstUser) {
             userColor = 'first-user-color';
             position = 'left';
+            triangle = 'triangle fas fa-caret-left fa-2x';
             //image links from API are broken, using placeholder img
             message.image = 'http://lorempixel.com/150/150/people/9';
           } else {
             userColor = 'second-user-color';
             position = 'right';
+            triangle = 'triangle fas fa-caret-right fa-2x';
             message.image = 'http://lorempixel.com/150/151/people/7';
           };
 
-          if (message.focused) focus = 'focus';
+          if (message.focused) {
+            focus = 'focus';
+            triangle += ' focus-triangle';
+          };
 
           return (
             <div className={`message-container ${position}`} key={i}>
               <img src={message.image}></img>
-              <Message message={message} focus={focus} userColor={userColor}/>
+              <i className={triangle}/>
+              <Message message={message} focus={focus} userColor={userColor} />
             </div>
           )
         })}
